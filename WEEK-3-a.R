f8b0c2a6ebe5db6cf16bec4824c2f29991b6ccc5
kodihema2345-byte <kodihema2345@gmail.com>
2026-01-29 10:40:05 +0530

    Update WEEK-3-a.R

--- WEEK-3-a.R
+++ WEEK-3-a.R
@@ -1,14 +1,55 @@
 data(iris)
 ?iris
-head(iris,3)
+#by default head shows 6 row , i want 10 rows
+head(iris ,10)
 str(iris)
 class(iris)
-#very basic bar chart
-#count of each species
-#to see the data in the coloumn "species"
+
 iris$Species
 View(iris)
-#ploiris#plotting a bar chart
 barplot(
-  table(iris$Species)
+  table(iris$Species),
+  main="court of Iris Species",
+  xlab ="Species",
+  ylab="Number of  Items",
+  col='steelblue'
+)
+#add title ,axis label , color
+#bar chart from aggregation data
+#using mean sepal length per species
+mean_sepal <- tapply(iris$Sepal.Length , iris$Species ,mean)
+mean_sepal
+
+barplot(
+  mean_sepal,
+  col='orange',
+  main="Average Sepal Length by Species",
+  xlab="Species",
+  ylab="Mean sepal Length"
+)
+
+ 
+#grouped bar chart
+#common Sepal&petal length
+
+group_mean <- rbind(
+  Sepal = tapply(iris$Sepal.Length , iris$Species ,mean),
+  petal =tapply(iris$Petal.Length , iris$Species ,mean)
+)
+group_mean
+#grouped cluster bar chart
+barplot(
+  group_mean,
+  beside =TRUE,
+  col= c("skyblue", "pink"),
+  legend.text =TRUE,
+  main="Clustered Bar chart :Sepal vs Petal"
+)
+#grouped stacked bar chart
+barplot(
+  group_mean,
+  beside =FALSE,
+  col= c("skyblue", "pink"),
+  legend.text =TRUE,
+  main="Stacked Bar chart :Sepal vs Petal"
 )
