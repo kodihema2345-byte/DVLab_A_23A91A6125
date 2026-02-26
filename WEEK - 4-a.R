7d2432ca7ca9d7a63e24840f219f7bd00a1f9a1b
Hema45 <kodihema2345@gmail.com>
2026-02-26 11:28:17 +0530

    WEEK-4-a

--- WEEK - 4-a.R
+++ WEEK - 4-a.R
@@ -9,3 +9,14 @@ boxplot(iris$Sepal.Length,
         main = "Box plot for Sepal Length",
         ylab = "Sepal_Length",
         col = 'skyblue')
+
+boxplot(Sepal.Length ~ Species,
+        data = iris,
+        main = "Box plot for Sepal Length",
+        ylab = "Sepal_Length",
+        xlab = "Species",
+        col = c('skyblue', 'green' , 'pink'))
+
+boxplot(iris,
+        main = "Multi Variable Box Plot",
+        col = c("red", 'skyblue', 'green' , 'pink'))
